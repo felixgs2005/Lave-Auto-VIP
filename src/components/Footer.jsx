80a68e470e342804fbc81490813124ee720dfbab
Félix Gray-Sylvain <felixgs2005@gmail.com>
2026-03-23 15:39:33 -0400

    Footer + npm install react-icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,58 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import { FaFacebookF, FaInstagram, FaTiktok } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer
-      style={{
-        backgroundColor: "#111",
-        color: "#fff",
-        padding: "1rem",
-        textAlign: "center",
-      }}
-    >
-      Footer OK
+    <footer className="footer-container">
+      <div className="footer-top">
+        <div className="footer-left">
+          <Link to="/">
+            <img
+              src="/images/logo-lave-auto-vip-club.png"
+              alt="logo-lavage-auto-VIP"
+              className="footer-logo"
+            />
+          </Link>
+          <div className="footer-socials">
+            <a
+              href="https://facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaFacebookF size={24} />
+            </a>
+            <a
+              href="https://instagram.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaInstagram size={24} />
+            </a>
+            <a
+              href="https://tiktok.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaTiktok size={24} />
+            </a>
+          </div>
+        </div>
+
+        <div className="footer-right"><Link to="/forfait">Forfait</Link>{" "}
+          <span className="gold-line">|</span>
+          <Link to="/realisation">Réalisations</Link>{" "}
+          <span className="gold-line">|</span>
+          <Link to="/#temoignage-section">Témoignages</Link>{" "}
+          <span className="gold-line">|</span>
+          <Link to="/contact">Contact</Link>
+        </div>
+      </div>
+
+      <div className="footer-bottom">
+        &copy; {new Date().getFullYear()} Tous droits réservés
+      </div>
     </footer>
   );
 };
